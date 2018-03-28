9c17d975ce0a9744b520e126d7c815b973327f0b
J.Lyu <jlyu@wpi.edu>
2018-03-28 19:43:07 -0400

    Initialize Understanding the Weird Parts folder. Add creation and hoisting.

---
+++ understanding-the-weird-parts/creation-and-hoisting.js
@@ -0,0 +1,23 @@
+// Hoisting: (can't really trust on...)
+
+// Not acturally moving code to the top of the page, but
+// before your code begin to be executed line by line, the JavaScript engine
+// has already set aside memory space for the variables that you've created 
+// in that entire code, and all of the functions you've created as well. So
+// those variables and funcitons exist in memory.
+
+// The difference between variables and functions is, the function its entirety
+// is placed into memory space, but when it comes to variables like 'a' below,
+// it doesn't know it's value until it starts executing its code, so instead, it
+// puts a placeholder called 'undefined'.
+// ** All variables in JavaScript are initially set to 'undefined'.
+
+b();	// 'called b!'
+console.log(a);	// undefined
+
+// if remove the line below, console.log(a); will return error 'a is not defined'
+var a = 'hello world!';
+
+function b() {
+	console.log('called b!');
+}
